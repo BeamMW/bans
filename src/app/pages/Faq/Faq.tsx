ffe0813f029eebac365d064999c445522b20d871
viktar <viktarmikhan@gmail.com>
2022-07-21 12:15:52 +0200

    block faq page

--- src/app/pages/Faq/Faq.tsx
+++ src/app/pages/Faq/Faq.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { PageTitle } from '../../components/PageTitle/PageTitle';
 import Accordion from '../../views/Accordion/Accordion';
 import About from '@app/views/About/About';
+import { Container, Flex, Heading } from 'theme-ui';
 const Faq: React.FC = () => {
   return (
     <>
       <PageTitle title="FAQ" />
-      <Accordion />
-    
-      <About/>
+      <Container>
+        <Flex sx={{ justifyContent: "center", alignItems:'center', alignContent:"center", minHeight: "50vh" }}>
+          <Heading>Coming soon...</Heading>
+        </Flex>
+      </Container>
+      {/* Waiting to be filled */}
+      {/* <Accordion />
+      <About/> */}
     </>
   )
 }
