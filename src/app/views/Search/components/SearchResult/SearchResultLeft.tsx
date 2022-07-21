c4e6a6a63bd5a50011daa843b7da14688dbcda55
Tigran <tigran.mkrtchyan2398@gmail.com>
2022-07-21 18:19:32 +0400

    fix font issue

--- src/app/views/Search/components/SearchResult/SearchResultLeft.tsx
+++ src/app/views/Search/components/SearchResult/SearchResultLeft.tsx
@@ -21,7 +21,7 @@ export const SearchResultLeft: React.FC<SearchResultLeftProps> = (props) => {
       <Flex>
         <Paragraph sx={{
               fontSize: '16px',
-              fontFamily: 'SF Pro Display',
+              fontFamily: 'SFProDisplay',
               fontWeight: 700,
               lineHeight: '19px',
               fontStyle: 'normal',
