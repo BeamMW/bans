18fedcee1099696170ba2839a0b8ce94a6c68f61
Tigran <tigran.mkrtchyan2398@gmail.com>
2022-07-28 16:42:52 +0400

    fix #61

--- src/app/components/Notification/Notifcation.tsx
+++ src/app/components/Notification/Notifcation.tsx
@@ -21,16 +21,18 @@ export const Notification = ({ notification, text, handler, closeHandler = null,
   }
 
   return (
-    <SplitContainer passKey={passKey} styles={{ background: "rgb(9 19 30)", /* position: "absolute", */ width: "100%", maxWidth: "630px"/* , marginTop: 10 * passKey */ }} leftWeight={11} rightWeight={1} height="59px" key={passKey}>
+    <SplitContainer passKey={passKey} styles={{ background: "rgb(9 19 30)", /* position: "absolute", */ width: "100%", maxWidth: "650px"/* , marginTop: 10 * passKey */ }} leftWeight={5} rightWeight={7} height="59px" key={passKey}>
       <Flex sx={{ lineHeight: '19px' }}>
-        <Text sx={{whiteSpace:'nowrap', flex:'7'}}>{text}</Text>
-        <span onClick={moreDetailsHandler} style={{ textDecoration: 'none', marginLeft: '14px', flex:5 }}>
-          <Text variant='link'>
+        <Text sx={{whiteSpace:'nowrap'}}>{text}</Text>
+      </Flex>
+      <Box>
+      <span onClick={moreDetailsHandler} style={{ textDecoration: 'none', marginLeft: '14px'}}>
+          <Text variant='link' sx={{mr: 3 }}>
             More Details
           </Text>
         </span>
-      </Flex>
       <RemoveIcon onClick={handleCloseNotification} />
+      </Box>
     </SplitContainer>
   )
 }
