b17d0dba48e159d5b4367260ca4cc7dd8b16101b
viktar <viktarmikhan@gmail.com>
2022-07-29 17:48:09 +0200

    change bans cid for testnet

--- src/app/constants/common.tsx
+++ src/app/constants/common.tsx
@@ -1,7 +1,5 @@
 
-//export const BANS_CID = 'a4733a5eb63b9ea8a3831d95ce26144a69e5a3fc48a881b2362be7de860f2956';
-//export const BANS_CID = 'be3a5707c08861330ea327741a50cc13cb8bc028de5338a8304d5d95a035f93d';
-export const BANS_CID = '560881a267df92b45d48a1dc6495fdd29b37878e1040b22b1c4f1ea13b467dc9';
+export const BANS_CID = '9636cb25af0a749c99edd8a4f80d09e8a1d289f56db7177fd7d9d3f641fad0b2';
 export const GROTHS_IN_BEAM = 100000000;
 export const ASSET = "BEAM";
 export const GT = "BEAMX";
