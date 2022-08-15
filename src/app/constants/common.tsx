b318334acadc9372a0326458538bab5d74dbbcb5
viktar <viktarmikhan@gmail.com>
2022-08-15 12:34:30 +0200

    added mainnet cid

--- src/app/constants/common.tsx
+++ src/app/constants/common.tsx
@@ -1,5 +1,5 @@
 
-export const BANS_CID = '9636cb25af0a749c99edd8a4f80d09e8a1d289f56db7177fd7d9d3f641fad0b2';
+export const BANS_CID = 'af4550f1f8a6051ffeffea06e0cb978f8076fdfc2101d2273d4e62c86540bc5e';
 export const GROTHS_IN_BEAM = 100000000;
 export const ASSET = "BEAM";
 export const GT = "BEAMX";
